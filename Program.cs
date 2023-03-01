462e3ebb75e2f7b24ba6a159aed22cc59be4396b
MohitMK0012 <100524313+MohitMK0012@users.noreply.github.com>
2023-03-01 11:18:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program {
+    public static void Main() {
+        Console.Write("\nEnter a positive integer: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string result = CountAndSay(n);
+        Console.WriteLine("\nCount and Say sequence: " + result +"\n");
+    }
+
+    public static string CountAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+
+        string previous = CountAndSay(n - 1);
+        string result = "";
+        char currentChar = previous[0];
+        int count = 1;
+
+        for (int i = 1; i < previous.Length; i++) {
+            if (previous[i] == currentChar) {
+                count++;
+            } else {
+                result += count.ToString() + currentChar;
+                currentChar = previous[i];
+                count = 1;
+            }
+        }
+
+        result += count.ToString() + currentChar;
+        return result;
+    }
+}
